53d434b027481e062c320bf350fecfb372f12d61
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2023-11-01 23:29:26 +0200

    Use Nuxt UI instead of Tailwind+HeadlessUI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    pages: true,
     srcDir: "src/",
     css: ["~/assets/css/main.css"],
-    postcss: {
-        plugins: {
-            tailwindcss: {},
-            autoprefixer: {}
+    modules: [
+        "@nuxt/ui",
+        "@nuxtjs/eslint-module"
+    ],
+    app: {
+        head: {
+            title: "modernbb",
+            charset: "utf-8",
+            viewport: "width=device-width, initial-scale=1"
         }
-    }
+    },
 });
