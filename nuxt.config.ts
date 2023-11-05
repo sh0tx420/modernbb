e0f585797953450cbb08d3eceac813a6101e76ed
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2023-11-05 18:38:52 +0200

    feat(config): Configure Tailwind, Google Fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
+    devtools: { enabled: false },
     pages: true,
     srcDir: "src/",
     css: ["~/assets/css/main.css"],
     modules: [
         "@nuxt/ui",
-        "@nuxtjs/eslint-module"
+        ["@nuxtjs/google-fonts", {
+            display: "swap",
+            download: true,
+            base64: true,
+            overwriting: true,
+            stylePath: "~/assets/css/googlefonts.css",
+            families: {
+                "Exo 2": [700],
+                "Rubik": [300, 400]
+            }
+        }]
     ],
     app: {
         head: {
@@ -15,4 +25,14 @@ export default defineNuxtConfig({
             viewport: "width=device-width, initial-scale=1"
         }
     },
+    tailwindcss: {
+        config: {
+            theme: {
+                fontFamily: {
+                    exo2: "Exo 2",
+                    rubik: "Rubik"
+                }
+            }
+        }
+    }
 });
