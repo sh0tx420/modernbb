9e228346e99b55af496cc3becd4931870aa6e8a1
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2023-11-13 16:45:59 +0200

    feat(frontend): Update navbar for i18n

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,23 @@
-<script setup>
+<script setup lang="ts">
+    // const { locale } = useI18n();
+
+    // labels correspond to $t keys
     const navUrls = [
         {
             link: "/",
-            label: "Home"
+            label: "navbar.home"
         },
         {
             link: "/settings",
-            label: "Settings"
+            label: "navbar.settings"
         }
     ]
 </script>
 
 <template>
-    <nav class="bg-neutral-700 mx-7 md:w-1/2 md:mx-auto mt-3 p-3 rounded-md shadow-md">
+    <nav class="dark:bg-neutral-800 mx-7 md:w-1/2 md:mx-auto mt-3 p-3 rounded-md shadow-md">
         <div class="flex flex-row justify-items-start gap-2">
-            <div class="flex text-xl font-exo2 font-bold items-center">
+            <div class="flex text-xl font-bold items-center">
                 <h1 class="text-purple-600">modern</h1>
                 <h1>bb</h1>
             </div>
@@ -23,14 +26,14 @@
                 <UDivider orientation="vertical" />
             </div>
 
-            <div class="flex flex-row gap-2 items-center font-rubik">
+            <div class="flex flex-row gap-2 items-center">
                 <ULink v-for="url in navUrls" :to="url.link" active-class="text-purple-500" inactive-class="transition-colors hover:text-purple-500">
-                    {{ url.label }}
+                    {{ $t(url.label) }}
                 </ULink>
             </div>
 
             <div class="flex ml-auto">
-                <UButton label="Login" size="md" color="purple" icon="i-heroicons-arrow-right-on-rectangle" :trailing="true" />
+                <UButton label="Login" size="md" color="primary" icon="i-heroicons-arrow-right-on-rectangle" :trailing="true" />
             </div>
         </div>
     </nav>
