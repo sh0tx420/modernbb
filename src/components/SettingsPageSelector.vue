b36d53d5f278b672acd2172e37e0a913439dcaf4
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2023-11-13 16:46:26 +0200

    feat(frontend): Create settings page selector component

---
+++ src/components/SettingsPageSelector.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+// https://ui.nuxt.com/elements/button#icon
+// ^ documentation about button icons
+const SettingsItems = [
+    {
+        categoryName: "Main",
+        buttons: [
+            {
+                label: "Site settings",
+                icon: "i-heroicons-adjustments-horizontal",
+                href: "/settings/main"
+            },
+            {
+                label: "whar 2",
+                icon: "хуй знает"
+            }
+        ]
+    },
+    {
+        categoryName: "cat 2",
+        buttons: [
+            {
+                label: "meow 3",
+                icon: "xd"
+            },
+            {
+                label: "meow 4",
+                icon: "no"
+            }
+        ]
+    }
+]
+</script>
+
+<template>
+    <aside class="fixed flex mt-2 max-w-fit bg-neutral-700 p-2 rounded-r-lg">
+        <ul class="flex flex-col list-none">
+            <li v-for="category in SettingsItems" class="flex flex-col">
+                <h3 class="text-neutral-400 font-mono">
+                    {{ category.categoryName }}
+                </h3>
+                <UButton v-for="btn in category.buttons" color="primary" variant="ghost" :icon="btn.icon" :to="btn.href">
+                    {{ btn.label }}
+                </UButton>
+            </li>
+        </ul>
+    </aside>
+</template>
