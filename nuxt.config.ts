94ab40a2e4374a398908965e9ff4e436756a7172
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2023-11-13 16:42:14 +0200

    feat(config): Add i18n configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     modules: [
         "@nuxt/ui",
         "@nuxtjs/color-mode",
+        "@nuxtjs/i18n",
         ["@nuxtjs/google-fonts", {
             display: "swap",
             base64: true,
@@ -23,6 +24,26 @@ export default defineNuxtConfig({
             charset: "utf-8",
             viewport: "width=device-width, initial-scale=1"
         }
+    },
+    i18n: {
+        locales: [
+            {
+                name: "English",
+                code: "en",
+                iso: "en",
+                file: "english.json"
+            },
+            {
+                name: "Русский",
+                code: "ru",
+                iso: "ru",
+                file: "russian.json"
+            }
+        ],
+        strategy: "no_prefix",
+        lazy: true,
+        langDir: "lang",
+        defaultLocale: "ru"
     },
     tailwindcss: {
         config: {
