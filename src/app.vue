dad750fa8c55434a87b2c040f033ee76d5180388
sh0tx420 <55631215+sh0tx420@users.noreply.github.com>
2023-11-13 16:43:45 +0200

    feat(frontend): Add app.vue for easier control of pages

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const bgColor = ref("#121212");
+
+function ChangeGlobalBgColor(color: string) {
+    document.body.style.backgroundColor = color;
+}
+
+/* <UInput class="max-w-sm m-2" v-model="bgColor" placeholder="backgroundColor" @input="ChangeGlobalBgColor(bgColor)" />*/
+
+// NOTE FOR SELF!!!! READ!!! IMPORTANT!!!
+// Make users able to modify a color palette, from which they can use the colors in navbar, background, primary color, etc.
+// and the option to style individual components
+</script>
+
+<template>
+    <div :style="{ backgroundColor: bgColor }" :ui="{ color: { primary: 'purple' } }">
+        <Navbar />
+        <LazyNuxtPage transition/>
+    </div>
+</template>
